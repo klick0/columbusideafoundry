00e96c2aec9b68f77da9c57e0b49ea68ae5d432d
Ethan Dicks <ethan.dicks@gmail.com>
2014-11-11 17:10:27 -0500

    Added working anim()
    Added functional anim() code to strobe the entire bar up and down (depending on current value) so that each coin drop produces a visual effect.

--- sketchbook/votingmachine_neopixel_pct/votingmachine_neopixel_pct.ino
+++ sketchbook/votingmachine_neopixel_pct/votingmachine_neopixel_pct.ino
@@ -10,6 +10,8 @@
  11-Sep-2014  erd  Modified for single logical strip of 180 pixels (3x60) on pin 5
  23-Oct-2014  erd  Mods for GCAC Open House
   3-Nov-2014  erd  Mods for percentage operation
+ 11-Nov-2014  erd  Added anim() to strobe selected bar every time a coin drops
+ 
 
 */
 #include <Adafruit_NeoPixel.h>
@@ -27,7 +29,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEN*3, PIN, NEO_GRB + NEO_KHZ800);
 // on a live circuit...if you must, connect GND first.
 
 #define INTENSITY 32
-#define SPEED 100
+#define SPEED 15
 
 #define BARMAX 8
 
@@ -52,6 +54,8 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEN*3, PIN, NEO_GRB + NEO_KHZ800);
 uint32_t L_color = 0;
 uint32_t M_color = 0;
 uint32_t R_color = 0;
+
+uint8_t logit = 0;
 
 int L_votes = 0;
 int M_votes = 0;
@@ -103,21 +107,23 @@ void loop() {
   R_drop = INVERT_TEST - digitalRead(R_button);
 
   if (L_drop) {
+      logit = 1;
       L_votes++;
-      //total_votes = L_votes+M_votes+R_votes;
-      //anim(LBAR, L_color, LOW, L_votes, total_votes);
+      anim(LBAR, L_color, LOW); //, L_votes, total_votes);
   }
   if (M_drop) {
+      logit = 1;
       M_votes++;
-      //total_votes = L_votes+M_votes+R_votes;
-      //anim(MBAR, M_color, HIGH, M_votes, total_votes);
+      anim(MBAR, M_color, HIGH); //, M_votes, total_votes);
   }
   if (R_drop) {
+      logit = 1;
       R_votes++;
-      //total_votes = L_votes+M_votes+R_votes;
-      //anim(RBAR, R_color, LOW, R_votes, total_votes);
+      anim(RBAR, R_color, LOW); //, R_votes, total_votes);
   }
   setbar(L_votes, M_votes, R_votes);
+  logit = 0;
+  
   delay(DEBOUNCE);  // software debounce
 }
 
@@ -130,53 +136,59 @@ void setbar(int lv, int mv, int rv)
   int remainder;
   float ratio;
 
-  Serial.print("L: ");
-  Serial.print(lv);
-  Serial.print("[");
-  if (vtotal)
-    Serial.print((lv * BARMAX) / vtotal);
-  else
-    Serial.print(0);
-  Serial.print("/");
-  Serial.print(BARMAX);
-  Serial.print("]");
-  Serial.print(" M: ");
-  Serial.print(mv);
-  Serial.print("[");
-  if (vtotal)
-    Serial.print((mv * BARMAX) / vtotal);
-  else
-    Serial.print(0);
-  Serial.print("/");
-  Serial.print(BARMAX);
-  Serial.print("]");
-  Serial.print(" R: ");
-  Serial.print(rv);
-  Serial.print("[");
-  if (vtotal)
-    Serial.print((rv * BARMAX) / vtotal);
-  else
-    Serial.print(0);
-  Serial.print("/");
-  Serial.print(BARMAX);
-  Serial.print("]");
-  Serial.print(" T: ");
-  Serial.println(lv+mv+rv);
+  if(logit) {
+    Serial.print("L: ");
+    Serial.print(lv);
+    Serial.print("[");
+    if (vtotal)
+      Serial.print((lv * BARMAX) / vtotal);
+    else
+      Serial.print(0);
+    Serial.print("/");
+    Serial.print(BARMAX);
+    Serial.print("]");
+    Serial.print(" M: ");
+    Serial.print(mv);
+    Serial.print("[");
+    if (vtotal)
+      Serial.print((mv * BARMAX) / vtotal);
+    else
+      Serial.print(0);
+    Serial.print("/");
+    Serial.print(BARMAX);
+    Serial.print("]");
+    Serial.print(" R: ");
+    Serial.print(rv);
+    Serial.print("[");
+    if (vtotal)
+      Serial.print((rv * BARMAX) / vtotal);
+    else
+      Serial.print(0);
+    Serial.print("/");
+    Serial.print(BARMAX);
+    Serial.print("]");
+    Serial.print(" T: ");
+    Serial.println(lv+mv+rv);
+  }
 
+  //
   // calculate left strip numbers
+  //
   if (vtotal)
     ratio = float(lv * BARMAX) / float(vtotal);
   else
     ratio = 0.0;
   cutoff = int(ratio);
   remainder = 100*float(ratio - cutoff);
-  Serial.print(" L ");
-  Serial.print(ratio);
-  Serial.print(" ");
-  Serial.print(cutoff);
-  Serial.print(" ");
-  Serial.println(remainder);
-
+  if (logit) {
+    Serial.print(" L ");
+    Serial.print(ratio);
+    Serial.print(" ");
+    Serial.print(cutoff);
+    Serial.print(" ");
+    Serial.println(remainder);
+  }
+  
   // draw main bar
   for(i=0; i < BARMAX; i++) {
     color = (i < cutoff ? L_color : 0);
@@ -194,15 +206,51 @@ void setbar(int lv, int mv, int rv)
     ratio = 0.0;
   cutoff = int(ratio);
   remainder = 100*float(ratio - cutoff);
-  Serial.print(" M ");
-  Serial.print(ratio);
-  Serial.print(" ");
-  Serial.print(cutoff);
-  Serial.print(" ");
-  Serial.println(remainder);
-
+  if (logit) {
+    Serial.print(" M ");
+    Serial.print(ratio);
+    Serial.print(" ");
+    Serial.print(cutoff);
+    Serial.print(" ");
+    Serial.println(remainder);
+  }
+  
   // draw main bar
   for(i=0; i < BARMAX - MIDDLE_SHORT; i++) {
+    color = (i < cutoff ? M_color : 0);
+      if (MIDDLE_REVERSED)
+        strip.setPixelColor((MBAR-i), color);
+      else
+        strip.setPixelColor(i+MBAR, color);
+  }
+  // draw half-lit bar topper
+  if ((cutoff < BARMAX - MIDDLE_SHORT) && (remainder > 0)) {
+    if (MIDDLE_REVERSED)
+      strip.setPixelColor(MBAR-cutoff, M_color / 4);
+    else
+      strip.setPixelColor(MBAR+cutoff, M_color / 4);
+  }
+
+ //
+  // calculate right strip numbers
+  //
+  if (vtotal)
+    ratio = float(rv * BARMAX) / float(vtotal);
+  else
+    ratio = 0.0;
+  cutoff = int(ratio);
+  remainder = 100*float(ratio - cutoff);
+  if (logit) {
+    Serial.print(" R ");
+    Serial.print(ratio);
+    Serial.print(" ");
+    Serial.print(cutoff);
+    Serial.print(" ");
+    Serial.println(remainder);
+  }
+  
+  // draw main bar
+  for(i=0; i < BARMAX; i++) {
     color = (i < cutoff ? R_color : 0);
     strip.setPixelColor(i+RBAR, color);
   }
@@ -213,22 +261,121 @@ void setbar(int lv, int mv, int rv)
 
   strip.show();
 }
-void anim(int start, uint32_t color, int invert, int votes, int total)
+void anim(int start, uint32_t color, int invert) //, int votes, int total)
 {
   uint8_t i;
+  int maxused=0;
 
-  for (i=0; i < BARMAX; i++) {
-    if (i < (BARMAX-1))
-      strip.setPixelColor(start+i, 0);  // black out one pixel above our blip
-    strip.setPixelColor(start+i, color);  // light up our blip
-    strip.show();
-    delay(SPEED);
+  // determine highest-used color in this bar
+  if (invert) {
+    while (strip.getPixelColor(start-maxused)==color)
+      maxused++;
   }
-  if (total < BARMAX) {
-    for (i=BARMAX-1; i > votes/total; --i) {
-        strip.setPixelColor(start+i, 0);
-        strip.show();
-        delay(SPEED);
+  else {
+    while (strip.getPixelColor(start+maxused))
+      maxused++;
+  }
+  
+  if (logit) {  
+    Serial.print("anim: ");
+    Serial.print(maxused);
+    Serial.print("+");
+    Serial.println(start);
+  }
+  
+  // inverted middle bar
+  if (invert) {
+    // count down the bar from maxused to bottom
+    if (maxused) {
+      for (i=maxused; i; --i) {
+          if (i <maxused)
+            if (MIDDLE_REVERSED)
+              strip.setPixelColor((start-i), color);
+            else
+              strip.setPixelColor(start+i, color);
+          if (MIDDLE_REVERSED)
+            strip.setPixelColor((start-i), 0);
+          else    
+            strip.setPixelColor(start+i, 0);
+          strip.show();
+          delay(SPEED);
+      }  
     }
+    // count up the bar from bottom to top
+    for (i=0; i < BARMAX - MIDDLE_SHORT; i++) {
+      if (i < (BARMAX-MIDDLE_SHORT-1)) {
+        if (MIDDLE_REVERSED)
+          strip.setPixelColor((start-i)-1, 0);
+        else
+          strip.setPixelColor(i+start+1, 0);
+      }
+      if (MIDDLE_REVERSED)
+        strip.setPixelColor((start-i), color);  // light up our blip
+      else
+        strip.setPixelColor(start+i, color);  // light up our blip
+      strip.show();
+      delay(SPEED);
+    }
+
+    // count down the bar from to the maxused
+    if (maxused < BARMAX - MIDDLE_SHORT) {
+      for (i=BARMAX-MIDDLE_SHORT-1; i > maxused; --i) {
+          if (MIDDLE_REVERSED)
+            strip.setPixelColor((start-i), 0);
+          else
+            strip.setPixelColor(start+i, 0);
+          strip.show();
+          delay(SPEED);
+      }
+    }
+    // special case for first vote - anim bottom to top
+    if (maxused == 0)
+    for (i = 0; i < BARMAX - MIDDLE_SHORT; i++) {
+      if (MIDDLE_REVERSED)
+        strip.setPixelColor((start-i), color);
+      else
+        strip.setPixelColor(start+i, color);
+      strip.show();
+      delay(SPEED);
+    }
+    
   }
+  // normal left and right bars
+  else {
+    // count down the bar from maxused to bottom
+    if (maxused) {
+      for (i=maxused; i; --i) {
+          if (i <maxused)
+            strip.setPixelColor(start+i, color);
+          strip.setPixelColor(start+i, 0);
+          strip.show();
+          delay(SPEED);
+      }
+    }
+    // count up the bar from bottom to top
+    for (i=0; i < BARMAX; i++) {
+      if (i < (BARMAX-1))
+        strip.setPixelColor(start+i+1, 0);  // black out one pixel above our blip
+      strip.setPixelColor(start+i, color);  // light up our blip
+      strip.show();
+      delay(SPEED);
+    }
+    
+    // count down the bar from top to the maxused
+    if (maxused < BARMAX) {
+      for (i=BARMAX-1; i > maxused; --i) {
+          strip.setPixelColor(start+i, 0);
+          strip.show();
+          delay(SPEED);
+      }
+    }
+
+    // special case for first vote - anim bottom to top
+    if (maxused == 0)
+    for (i = 0; i < BARMAX; i++) {
+      strip.setPixelColor(start+i, color);
+      strip.show();
+      delay(SPEED);
+    }
+  }  
 }
